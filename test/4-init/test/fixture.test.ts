fa807c48d920d05c9742d254eea8a190864bf5c0
Charlie Grund <kh.hansson@gmail.com>
2025-02-19 12:55:41 +0100

    Wip

--- test/4-init/test/fixture.test.ts
+++ test/4-init/test/fixture.test.ts
@@ -1,7 +1,55 @@
 import { runFixture } from 'test/__factory';
+import { describe } from 'vitest';
 
-runFixture({
-    name: 'init',
-    runArgs: ['--init'],
-    packages: {},
+describe('Init', () => {
+    runFixture({
+        name: 'init',
+        runArgs: ['--init'],
+        packages: {
+            '.': {
+                linkedFileNames: {
+                    included: [
+                        '.vscode',
+                        '.gitignore',
+                        '.dockerignore',
+                        '.env.development',
+                        '.env.production',
+                        '.eslintignore',
+                        '.eslintrc.json',
+                        '.folder',
+                        '.folder2',
+                        '.gitattributes',
+                        '.github',
+                        '.npmrc',
+                        'CONTRIBUTING.md',
+                        'README.md',
+                        'commitlint.config.js',
+                        'pnpm-lock.yaml',
+                        'pnpm-workspace.yaml',
+                        'renovate.json',
+                        'tsconfig.base.json',
+                        'tsconfig.build.json',
+                        'tsconfig.eslint.json',
+                        'tsconfig.spec.json',
+                        'turbo.json',
+                        'vite.config.ts',
+                        'vitest.config.ts',
+                    ],
+                    excluded: ['package.json'],
+                },
+            },
+            'src/nested/folder': {
+                linkedFileNames: {
+                    included: ['.env.development', 'tsconfig.json', 'turbo.json'],
+                    excluded: ['package.json'],
+                },
+            },
+            'src/nested/project': {
+                linkedFileNames: {
+                    included: ['tsconfig.json', 'vite.config.ts', 'vitest.config.ts'],
+                    excluded: ['package.json'],
+                },
+            },
+        },
+    });
 });
